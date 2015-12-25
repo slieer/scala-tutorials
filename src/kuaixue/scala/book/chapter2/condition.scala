327eb491675a8f46d07ef8d3961dfbac49771983
zhai <slieer@gmail.com>
2015-12-25 08:03:03 +0800

    sentence stop test.

--- src/kuaixue/scala/book/chapter2/condition.scala
+++ src/kuaixue/scala/book/chapter2/condition.scala
@@ -6,5 +6,12 @@ object condition extends App{
   
   val s = if(x > 0) 1 else -1
   
+  val s1 = if(x > 0) "positive" else -1
   
+  def equalsExpression(){
+	  val s = if(x > 0)  1
+    val s1 = if(x > 0) 1 else ()
+    val s2 = if(x > 0) 1 else Unit
+  }
+    
 }
