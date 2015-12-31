8bffea7e1604a75123fc31129157275ce57bdbeb
zhai <slieer@gmail.com>
2015-12-31 08:00:25 +0800

    collect test, ch14, ch20 test.

--- src/collect/immutable/ListTest.scala
+++ src/collect/immutable/ListTest.scala
@@ -1,4 +1,5 @@
-package collect.immutable/**不可变对象序列List*/
+package collect.immutable
+/**不可变对象序列List*/
 
 object ListTest extends App {
   val oneTwo = List[Int](1, 2)
@@ -11,40 +12,62 @@ object ListTest extends App {
 
   val twoThree = List(2, 3)
   val oneTwoThree = 1 :: twoThree
-  
+
   println(oneTwoThree)
 
   val oneTwoThree_ = andStr :: 1 :: 2 :: 3 :: Nil
   println(oneTwoThree_)
-  
-  
+
   var o = List[Int](1, 2, 3)
   var o1 = List[Int](4, 5, 6)
   var o2 = o ::: o1
   println(o2)
-  
+
   var o3 = o :: o1
   println(o3)
-  
+
   println(List("a", "b") ::: List("c", "d"))
   println(List("a", "b") :: List("c", "d"))
-  
+
   var thrill = "Cool" :: "tools" :: "rule" :: Nil
   println(thrill(2))
-  
-  println(thrill.count { thizStr => thizStr.length()==4 })
-  
+
+  println(thrill.count { thizStr => thizStr.length() == 4 })
+
   thrill.foreach { x => print(x + ",") }
   thrill.foreach(s => print(s))
   thrill.foreach(print)
-  
+
   println(thrill.exists { x => x.contains("o") })
-  
+
   println(thrill.filter(s => s.length == 4))
-  
+
   thrill.foreach(s => {
     var ss = s + "x"
     print(ss.+(","))
   })
+
+  val digits = List(4, 2)
+  var add = 9 :: digits
+
+  9 :: 4 :: 2 :: Nil
+  9 :: (4 :: (2 :: Nil))
+
+  def sum(lst: List[Int]): Int =
+    if (lst == Nil) 0 else lst.head + sum(lst.tail)
+  //as same as
+  def sum_v1(lst: List[Int]): Int = lst match {
+    case Nil => 0
+    case h :: t => h + sum_v1(t)
+  }
+
+  digits.sum
+  digits contains 1
   
+  val names = List("Peter", "Paul", "Marry")
+  names.map { x => x.toLowerCase() }
+  //as same as
+  names.map (_.toLowerCase)
+  //as same as
+  for(n <- names)yield n.toUpperCase()
 }
